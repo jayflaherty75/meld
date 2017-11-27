833aa8fef06bbefcfac67771899fe5de93cc997f
Jason K. Flaherty <coderx75@hotmail.com>
2017-11-27 13:02:13 -0600

    Updated windows build

--- build.bat
+++ build.bat
@@ -4,7 +4,61 @@
 ECHO Starting Meld Core build...
 ECHO.
 
-fbc -mt -C "./src/fb/tester/tester.bas" -x "./tester.exe"
+fbc -mt -c "./src/fb/meld/lib/fault/fault.bas"
+
+IF %ERRORLEVEL% NEQ 0 (
+	ECHO.
+	ECHO Failed to compile fault, build terminated
+	EXIT /B 1
+)
+ 
+fbc -mt -c "./src/fb/shared/lib/bst/bst.bas"
+
+IF %ERRORLEVEL% NEQ 0 (
+	ECHO.
+	ECHO Failed to compile bst, build terminated
+	EXIT /B 1
+)
+ 
+fbc -mt -c "./src/fb/shared/lib/identity/identity.bas"
+
+IF %ERRORLEVEL% NEQ 0 (
+	ECHO.
+	ECHO Failed to compile identity, build terminated
+	EXIT /B 1
+)
+ 
+fbc -mt -c "./src/fb/shared/lib/iterator/iterator.bas"
+
+IF %ERRORLEVEL% NEQ 0 (
+	ECHO.
+	ECHO Failed to compile iterator, build terminated
+	EXIT /B 1
+)
+ 
+fbc -mt -c "./src/fb/shared/lib/list/list.bas"
+
+IF %ERRORLEVEL% NEQ 0 (
+	ECHO.
+	ECHO Failed to compile list, build terminated
+	EXIT /B 1
+)
+ 
+fbc -mt -c "./src/fb/shared/lib/paged-array/paged-array.bas"
+
+IF %ERRORLEVEL% NEQ 0 (
+	ECHO.
+	ECHO Failed to compile paged-array, build terminated
+	EXIT /B 1
+)
+ 
+fbc -mt -C "./src/fb/tester/tester.bas" -x "./tester.exe" ^
+	"./src/fb/meld/lib/fault/fault.o" ^
+	"./src/fb/shared/lib/bst/bst.o" ^
+	"./src/fb/shared/lib/identity/identity.o" ^
+	"./src/fb/shared/lib/iterator/iterator.o" ^
+	"./src/fb/shared/lib/list/list.o" ^
+	"./src/fb/shared/lib/paged-array/paged-array.o"
 
 IF %ERRORLEVEL% NEQ 0 (
 	ECHO.
@@ -25,7 +79,13 @@ IF %ERRORLEVEL% NEQ 0 (
 ECHO.
 ECHO Building Meld Core...
 
-fbc -mt -C "./src/fb/meld/meld.bas" -x "./meld.exe"
+fbc -mt -C "./src/fb/meld/meld.bas" -x "./meld.exe" ^
+	"./src/fb/meld/lib/fault/fault.o" ^
+	"./src/fb/shared/lib/bst/bst.o" ^
+	"./src/fb/shared/lib/identity/identity.o" ^
+	"./src/fb/shared/lib/iterator/iterator.o" ^
+	"./src/fb/shared/lib/list/list.o" ^
+	"./src/fb/shared/lib/paged-array/paged-array.o"
 
 IF %ERRORLEVEL% NEQ 0 (
 	ECHO.
