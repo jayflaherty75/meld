e0d92815e70701cd1363fa00a0627176777cc5b8
Jason K. Flaherty <coderx75@hotmail.com>
2018-03-18 14:26:43 -0500

    RM-49 - Build tools batch file and removed temp files from repo

---
+++ build/build-tools.bat
@@ -0,0 +1,4 @@
+
+fbc -C -s console "./build/tools/parse-fb/main.bas" -x "./build/parse-fb.exe"
+fbc -C -s console "./build/tools/parse-cpp/main.bas" -x "./build/parse-cpp.exe"
+fbc -C -s console "./build/tools/kebab-to-pascal/main.bas" -x "./build/kebab-to-pascal.exe"
